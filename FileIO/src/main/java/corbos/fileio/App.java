b71e9e2af4a9d9368ae6fabb8b0c4fa484c112ef
corbos <corbin.march@gmail.com>
2017-05-15 22:49:37 -0500

    FileIO demo project

---
+++ FileIO/src/main/java/corbos/fileio/App.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package corbos.fileio;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author parallels
+ */
+public class App {
+
+    private static final String PATH = "data.txt";
+
+    public static void main(String[] args) {
+        displayFile(PATH); // file is relative to the project, so this should work.
+        addRecord("Ernie", "Pittman", "ernie.pittman@aol.com", "1111222299998888");
+        displayFile(PATH); // display a second time to confirm our record was added.
+    }
+
+    private static void displayFile(String path) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line;
+            reader.readLine(); // throw out header...
+            int recordNumber = 0;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split(",");
+                System.out.printf("Record: %s, Name: %s %s, Email: %s\n", recordNumber++, words[0], words[1], words[2]);
+
+            }
+            reader.close(); // files must be closed explicitly.
+        } catch (IOException ex) {
+            System.out.printf("Error!: %s", ex);
+        }
+    }
+
+    private static void addRecord(String firstName, String lastName, String email, String creditCard) {
+        try (FileWriter fw = new FileWriter(PATH, true); // the second parameter indicates we append text vs start from scratch
+                BufferedWriter bw = new BufferedWriter(fw);
+                PrintWriter writer = new PrintWriter(bw);) {
+            writer.printf("\n%s,%s,%s,%s", firstName, lastName, email, creditCard);
+            writer.flush();
+            writer.close();
+        } catch (IOException ex) {
+            System.out.printf("Error!: %s", ex);
+        }
+    }
+}
